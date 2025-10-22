802b33a9d6d7ad2128cbe0f6c4d1d8548d5e6dc1
ahmadfauzanarif <ahmadfauzanarif3@gmail.com>
2025-10-22 08:49:32 +0700

    Praktikum 2 - update fitur pengaduan

--- warga/views.py
+++ warga/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from django.views.generic import DetailView
-from .models import Warga
+from .models import Warga,Pengaduan
 # Create your views here.
 
 
 class WargaListView(ListView):
     model = Warga
     template_name = 'warga/warga_list.html'
+
+class PengaduanListView(ListView):
+    model = Pengaduan
+    template_name = 'warga/pengaduan_list.html'
+    context_object_name = 'pengaduan_list'
+    ordering = ['-tanggal_lapor']  # urutkan dari yang terbaru
 
 
 class WargaDetailView(DetailView):
